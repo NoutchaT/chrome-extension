25fb5a7b84c1bdc26d6493f3314fd6a76ca185b1
NoutchaT <noutcha.tchatat@edu.devinci.fr>
2020-12-16 16:03:28 +0100

    first commit

---
+++ src/components/App.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Profile } from "../models/profile";
+import "./App.scss";
+
+interface State extends Profile {}
+
+export class App extends React.Component<{}, State> {
+  state = {} as State;
+
+  public componentDidMount() {
+    if (chrome && chrome.tabs) {
+      chrome.tabs.query({ currentWindow: true, active: true }, tabs => {
+        const tab = tabs[0];
+        chrome.tabs.sendMessage(tab.id || 0, { from: "popup", subject: "getFullName" }, response => {
+          this.setState({
+            fullName: response.fullName,
+            title: response.title,
+            country: response.country,
+            imageUrl: response.imageUrl
+          });
+        });
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <div>{this.state.fullName}</div>
+        <div>{this.state.title}</div>
+        <div>{this.state.country}</div>
+        <img src={this.state.imageUrl} alt="profileImage"></img>
+      </div>
+    );
+  }
+}
+
+export default App;
